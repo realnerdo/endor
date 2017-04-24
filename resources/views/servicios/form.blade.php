378db629ceeb3579e7d3c483f282efee4f997777
Asael Jaimes <asaelx@gmail.com>
2017-04-24 16:06:15 -0500

    Remove content field from services

--- resources/views/servicios/form.blade.php
+++ resources/views/servicios/form.blade.php
@@ -8,76 +8,70 @@
     {{ Form::input('text', 'price', null, ['class' => 'input']) }}
 </div>
 <!-- /.form-group -->
-{{-- <div class="form-group"> --}}
-<div class="form-group">
-    {{ Form::label('content', 'Contenido', ['class' => 'label']) }}
-    {{ Form::textarea('content', null, ['size' => '30x5', 'class' => 'input autosizable']) }}
-</div>
-<!-- /.form-group -->
-    <div class="row">
-        <div class="col-12">
-            <h3 class="title">Secciones</h3><!-- /.title -->
-        </div>
-        <!-- /.col-12 -->
-        <div class="sections_list">
-            @if($service->sections->isEmpty())
+
+<div class="row">
+    <div class="col-12">
+        <h3 class="title">Secciones</h3><!-- /.title -->
+    </div>
+    <!-- /.col-12 -->
+    <div class="sections_list">
+        @if($service->sections->isEmpty())
+            <div class="section row">
+                <div class="col-12">
+                    <div class="form-group">
+                        {{ Form::label('sections[0][title]', 'Título', ['class' => 'label']) }}
+                        {{ Form::input('text', 'sections[0][title]', null, ['class' => 'input']) }}
+                    </div>
+                    <!-- /.form-group -->
+                    <div class="form-group">
+                        {{ Form::label('sections[0][content]', 'Contenido', ['class' => 'label']) }}
+                        {{ Form::textarea('sections[0][content]', null, ['size' => '30x5', 'class' => 'input autosizable']) }}
+                    </div>
+                    <!-- /.form-group -->
+                </div>
+                <!-- /.col-12 -->
+            </div>
+            <!-- /.section row -->
+        @else
+            @foreach ($service->sections as $key => $section)
                 <div class="section row">
                     <div class="col-12">
                         <div class="form-group">
-                            {{ Form::label('sections[0][title]', 'Título', ['class' => 'label']) }}
-                            {{ Form::input('text', 'sections[0][title]', null, ['class' => 'input']) }}
+                            {{ Form::label('sections[' . $key . '][title]', 'Título', ['class' => 'label']) }}
+                            {{ Form::input('text', 'sections[' . $key . '][title]', $section->title, ['class' => 'input']) }}
                         </div>
                         <!-- /.form-group -->
                         <div class="form-group">
-                            {{ Form::label('sections[0][content]', 'Contenido', ['class' => 'label']) }}
-                            {{ Form::textarea('sections[0][content]', null, ['size' => '30x5', 'class' => 'input autosizable']) }}
+                            {{ Form::label('sections[' . $key . '][content]', 'Contenido', ['class' => 'label']) }}
+                            {{ Form::textarea('sections[' . $key . '][content]', $section->content, ['size' => '30x5', 'class' => 'input autosizable']) }}
                         </div>
                         <!-- /.form-group -->
                     </div>
                     <!-- /.col-12 -->
+                    @if($key != 0)
+                        <div class="col-12">
+                            <button class="btn btn-red delete-section">Eliminar</button>
+                        </div><!-- /.col-12 -->
+                    @endif
                 </div>
                 <!-- /.section row -->
-            @else
-                @foreach ($service->sections as $key => $section)
-                    <div class="section row">
-                        <div class="col-12">
-                            <div class="form-group">
-                                {{ Form::label('sections[' . $key . '][title]', 'Título', ['class' => 'label']) }}
-                                {{ Form::input('text', 'sections[' . $key . '][title]', $section->title, ['class' => 'input']) }}
-                            </div>
-                            <!-- /.form-group -->
-                            <div class="form-group">
-                                {{ Form::label('sections[' . $key . '][content]', 'Contenido', ['class' => 'label']) }}
-                                {{ Form::textarea('sections[' . $key . '][content]', $section->content, ['size' => '30x5', 'class' => 'input autosizable']) }}
-                            </div>
-                            <!-- /.form-group -->
-                        </div>
-                        <!-- /.col-12 -->
-                        @if($key != 0)
-                            <div class="col-12">
-                                <button class="btn btn-red delete-section">Eliminar</button>
-                            </div><!-- /.col-12 -->
-                        @endif
-                    </div>
-                    <!-- /.section row -->
-                @endforeach
-            @endif
-
-        </div>
-        <!-- /.sections_list -->
-
-        <div class="col-12">
-            <div class="buttons pr">
-                <button class="btn btn-blue add-section"><i class="typcn typcn-plus"></i> Añadir otra sección</button>
-            </div>
-            <!-- /.buttons pr -->
-        </div>
-        <!-- /.col-12 -->
+            @endforeach
+        @endif
 
     </div>
-    <!-- /.row -->
-{{-- </div> --}}
-<!-- /.form-group -->
+    <!-- /.sections_list -->
+
+    <div class="col-12">
+        <div class="buttons pr">
+            <button class="btn btn-blue add-section"><i class="typcn typcn-plus"></i> Añadir otra sección</button>
+        </div>
+        <!-- /.buttons pr -->
+    </div>
+    <!-- /.col-12 -->
+
+</div>
+<!-- /.row -->
+
 <div class="form-group">
     {{ Form::label('notes', 'Cláusulas de contratación', ['class' => 'label']) }}
     {{ Form::textarea('notes', null, ['size' => '30x5', 'class' => 'input autosizable']) }}
